3fb80e2b9073dfa0cbe0d6329ab1c00ae29cbb3a
isabellebeutelspacher <isabelle.beutelspacher@stundent.uibk.ac.at>
2019-03-25 09:47:24 +0100

    typeof operator 9/10

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,3 +1,4 @@
-let myName = 'Natalia';
-let myCity = 'Mexico City';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
