a4e5383e92319e46079a814eca87f489586324fa
Beutelspacher <csav8110@uibk.ac.at>
2019-03-27 18:53:25 +0100

    Karte vervollständigen

--- adler/main.js
+++ adler/main.js
@@ -1,10 +1,10 @@
 // alert("Hallo Welt!");
 
 const div = document.getElementById("map");
-const breite1= div.getAttribute("data-lat1");
+const breite1 = div.getAttribute("data-lat1");
 const laenge1 = div.getAttribute("data-lng1");
 const titel1 = div.getAttribute("data-title1");
-const breite2= div.getAttribute("data-lat2");
+const breite2 = div.getAttribute("data-lat2");
 const laenge2 = div.getAttribute("data-lng2");
 const titel2 = div.getAttribute("data-title2");
 
@@ -18,22 +18,94 @@ let karte = L.map("map");
 // console.log(Karte);
 
 // auf Ausschnitt zoomen, Zoomlevel 13
-karte.setView(
-    [breite1,laenge1], 
+/*karte.setView(
+    [breite1, laenge1],
     12
-);
+);*/
 
 //openstreetmap einbauen miz z=Zoom, x=Länge und y=Breite, s=server
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 //Positionsmarker hinzufügen
 let pin1 = L.marker(
-    [breite1,laenge1]
+    [breite1, laenge1]
 ).addTo(karte);
 let pin2 = L.marker(
-    [breite2,laenge2]
+    [breite2, laenge2]
 ).addTo(karte);
 
 //Popup zum Pin hängen
 pin1.bindPopup(titel1).openPopup();
 pin2.bindPopup(titel2).openPopup();
+
+
+/*const blick1 = {
+    kunde: "Wilder Kaiser",
+    standort: "Gruttenhütte",
+    seehoehe: 1640,
+    lat: 47.55564,
+    lng: 12.31861,
+};
+let pin3 = L.marker(
+    [blick1.lat, blick1.lng]
+).addTo(karte);
+pin3.bindPopup(
+    `<h1>Standort: ${blick1.standort}</h1>
+    <p>Höhe: ${blick1.seehoehe} m</p>
+    <em>Kunde: ${blick1.kunde}</em>`
+);
+
+const blick2 = {
+    kunde: "Bergbahn Scheffau",
+    standort: "Brandstadl",
+    seehoehe: 1640,
+    lat: 47.4912,
+    lng: 12.248,
+};
+
+const blick3 = {
+    kunde: "Lechtal Tourismus",
+    standort: "Sonnalm Jöchelspitze",
+    seehoehe: 1786,
+    lat: 47.28103,
+    lng: 10.37508,
+};
+
+const adlerblicke = [
+    {
+        kunde: "Wilder Kaiser",
+        standort: "Gruttenhütte",
+        seehoehe: 1640,
+        lat: 47.55564,
+        lng: 12.31861,
+    },
+    {
+        kunde: "Bergbahn Scheffau",
+        standort: "Brandstadl",
+        seehoehe: 1640,
+        lat: 47.4912,
+        lng: 12.248,
+    },
+    {
+        kunde: "Lechtal Tourismus",
+        standort: "Sonnalm Jöchelspitze",
+        seehoehe: 1786,
+        lat: 47.28103,
+        lng: 10.37508,
+    }
+];*/
+
+let blickeGruppe=L.featureGroup().addTo(karte);
+
+for (let blick of ADLERBLICKE) {
+    let blickpin = L.marker(
+        [blick.lat, blick.lng]
+    ).addTo(blickeGruppe);
+    blickpin.bindPopup(
+        `<h1>Standort: ${blick.standort}</h1>
+        <p>Höhe: ${blick.seehoehe} m</p>
+        <em>Kunde: ${blick.kunde}</em>`
+    )
+}
+console.log(blickeGruppe.getBounds()); //zeig nur in Konsole an
+karte.fitBounds(blickeGruppe.getBounds()); //Auf alle Marker zommen
