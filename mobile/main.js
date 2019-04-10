c4bf4bc51958899d64da0c96a70fece3d659d4eb
Beutelspacher <csav8110@uibk.ac.at>
2019-04-10 17:16:12 +0200

    Watch rausgenommen

--- mobile/main.js
+++ mobile/main.js
@@ -75,7 +75,7 @@ let positionsMarker = L.marker([47, 11]).addTo(karte);
 karte.locate({
     setView: true,
     maxZoom: 16,
-    watch: true,
+    //watch: true,
 });
 
 karte.on("locationfound", function (event) {
